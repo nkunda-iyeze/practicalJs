e9d3b2559486c05640a270ec16d5a37a22a5de13
parlom <shalom.iyeze@gmail.com>
2022-05-21 13:12:46 +0200

    Jvascript review on first chapter

--- passengerApp/index.js
+++ passengerApp/index.js
@@ -14,9 +14,73 @@ function save() {
     savEl.textContent += saveCount;
     count = 0;
     countEl.textContent = count;
-    console.log(saveCount)
+    // console.log(saveCount)
 }
 let username = "shalom"
 let message = "You have three new Notifications ";
 // console.log(message);
 // save();
+
+
+// ------------------------------    CHALLENGE FOR QUICK REVIEW OF WHAT WE STUDIED   ------------------
+// exercise 1;
+let firstName = "Nkunda Iyeze";
+let lastName = "Shalom";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// exercise 2;
+
+let name = "linda";
+let greeting = "Hi, there";
+
+function greet() {
+    console.log(greeting + " " + name + "!");
+}
+
+greet()
+
+// exercise 3
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Point() {
+    myPoints--;
+}
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+
+// console.log(myPoints)
+// small calculator
+let num1 = document.getElementById("num1-el").textContent = 18;
+let num2 = document.getElementById("num2-el").textContent = 8;
+// console.log(num1);
+let result = 0;
+let sumEl = document.getElementById("sum-el");
+
+function add() {
+    result = num1 + num2;
+    sumEl.textContent += result + " - ";
+    // break;
+}
+
+function sub() {
+    result = num1 - num2;
+    sumEl.textContent += result + " - ";
+}
+
+function mult() {
+    result = num1 * num2;
+    sumEl.textContent += result + " - ";
+}
+
+function div() {
+    result = num1 / num2;
+    sumEl.textContent += result + " - ";
+}
--- passengerApp/passenger.js
+++
@@ -1,7 +0,0 @@
-let welcomeEl = document.getElementById("welcome-el");
-let name = "Nkunda Iyeze Shalom";
-let greetings = "Welcome back, " + name;
-
-welcomeEl.innerText = greetings;
-
-welcomeEl.innerText += " ✊🏾"
