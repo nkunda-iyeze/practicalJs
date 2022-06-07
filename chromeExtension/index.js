216d5a257007688d00fb759894df34ee30587843
parlom <shalom.iyeze@gmail.com>
2022-06-08 01:17:58 +0200

    clear the input field

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -4,6 +4,7 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
+    inputEl.value = "";
     renderLeads();
 });
 
