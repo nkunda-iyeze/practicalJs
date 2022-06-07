3a5083325c5c2e30086ab31aa331f182a7178348
parlom <shalom.iyeze@gmail.com>
2022-06-08 01:08:13 +0200

    renderLeads function created

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,34 +1,18 @@
-let myLeads = ["Good Morning", "Bonjour", "Mwaramutse", "Dobriden"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
-    // console.log("Add Event listener clicked !🥰");
     myLeads.push(inputEl.value);
-    console.log(myLeads)
+    renderLeads();
 });
 
-let listItems = "";
+function renderLeads() {
+    let listItems = "";
 
-for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    listItems += "<li>" + myLeads[i] + "</li>";
-
-    console.log(listItems);
-    // // create an element
-    // const li = document.createElement("li");
-    // // set text content
-    // li.textContent = myLeads[i];
-    // // append
-    // ulEl.append(li);
-
+    for (let i = 0; i < myLeads.length; i++) {
+        // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
-
-// practice 
-// const container = document.getElementById("container");
-// container.innerHTML = "<button onclick=buy()> Buy ! </button> ";
-
-// function buy() {
-// container.innerHTML += "<p> Thank you for buying with us ! </p>"
-// }
