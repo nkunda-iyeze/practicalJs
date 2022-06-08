3dea60cae631e3489142227743ca009c8986016a
parlom <shalom.iyeze@gmail.com>
2022-06-08 02:08:56 +0200

    a tag added to the string

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -13,7 +13,7 @@ function renderLeads() {
 
     for (let i = 0; i < myLeads.length; i++) {
         // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-        listItems += "<li>" + myLeads[i] + "</li>";
+        listItems += "<li> <a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
     }
     ulEl.innerHTML = listItems
 }
