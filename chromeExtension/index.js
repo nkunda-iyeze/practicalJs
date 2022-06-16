fbf8e8afebc23ce99a7afa5913bfe31709cee17a
parlom <shalom.iyeze@gmail.com>
2022-06-17 01:36:34 +0200

    first look on localStorage

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -2,6 +2,12 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+
+
+
+console.log(localStorage.getItem("myLeads"));
+// localStorage.clear();
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     inputEl.value = "";
