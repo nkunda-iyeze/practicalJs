19eda5b482be6af74a59fe9faa9bfd0164725776
parlom <shalom.iyeze@gmail.com>
2022-06-17 02:17:07 +0200

    save the Leads to the localStorage

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,21 +1,23 @@
-let myLeads = `["www.bestwing.org.com"]`;
-myLeads = JSON.parse(myLeads);
-myLeads.push("www.google.com");
-myLeads = JSON.stringify(myLeads);
-console.log(typeof myLeads);
+let myLeads = [];
+// myLeads = JSON.parse(myLeads);
+// myLeads.push("www.google.com");
+// myLeads = JSON.stringify(myLeads);
+// console.log(typeof myLeads);
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-
-
-console.log(localStorage.getItem("myLeads"));
+// console.log(localStorage.getItem("myLeads"));
 // localStorage.clear();
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+
+    localStorage.setItem("myLeads", `${JSON.stringify(myLeads)}`);
     renderLeads();
+    console.log(localStorage.getItem("myLeads"));
+
 });
 
 function renderLeads() {
